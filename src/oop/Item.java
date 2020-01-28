6700146d83c8eb37a12093ebd1eb4bb238bc3f4e
Julianne Catalan <Julianne Catalan@LAPTOP-0LLPCM77>
2020-01-28 09:25:35 +0800

    version 0.9.1

---
+++ src/oop/Item.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop;
+
+/**
+ *
+ * @author Julianne Catalan
+ */
+public abstract class Item {
+    private String itemtype;
+    private int itemcount;
+    
+    public Item(String itemtype, int itemcount){
+        this.itemtype= itemtype;
+        this.itemcount= itemcount;
+    }
+
+    public String getItemtype() {
+        return itemtype;
+    }
+
+    public int getItemcount() {
+        return itemcount;
+    }
+
+    public void setItemtype(String itemtype) {
+        this.itemtype = itemtype;
+    }
+
+    public void setItemcount(int itemcount) {
+        this.itemcount = itemcount;
+    }
+    
+}
